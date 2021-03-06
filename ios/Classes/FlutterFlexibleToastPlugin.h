58cc2ad9da5a0366b45482a81d8c5aa9c8f54ade
kw2019ltd <kw.2019.ltd@gmail.com>
2021-03-06 12:19:41 +0200

    migrate to null safty

--- ios/Classes/FlutterFlexibleToastPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterFlexibleToastPlugin : NSObject<FlutterPlugin>
-@end
--- lib/flutter_flexible_toast.dart
+++ lib/flutter_flexible_toast.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -22,92 +21,85 @@ enum ICON {
 }
 
 class FlutterFlexibleToast {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_flexible_toast');
+  static const MethodChannel _channel = MethodChannel('flutter_flexible_toast');
 
-  static Future<bool> cancel() async {
-    bool res = await _channel.invokeMethod("cancel");
+  static Future<bool?> cancel() async {
+    var res = await _channel.invokeMethod('cancel');
     return res;
   }
 
-  static Future<bool> showToast({
-    @required String message,
-    Toast toastLength,
-    int timeInSeconds,
-    double fontSize,
-    ToastGravity toastGravity,
-    Color backgroundColor,
-    Color textColor,
-    ICON icon,
-    int radius,
-    int elevation,
-    int imageSize,
+  static Future<bool?> showToast({
+    required String message,
+    Toast? toastLength,
+    int? timeInSeconds,
+    double? fontSize,
+    ToastGravity? toastGravity,
+    Color? backgroundColor,
+    Color? textColor,
+    ICON? icon,
+    int? radius,
+    int? elevation,
+    int? imageSize,
   }) async {
-    String toast = "short";
+    var toast = 'short';
     if (toastLength == Toast.LENGTH_LONG) {
-      toast = "long";
+      toast = 'long';
     }
-    String gravityToast = "bottom";
+    var gravityToast = 'bottom';
     if (toastGravity == ToastGravity.TOP) {
-      gravityToast = "top";
+      gravityToast = 'top';
     } else if (toastGravity == ToastGravity.CENTER) {
-      gravityToast = "center";
+      gravityToast = 'center';
     } else {
-      gravityToast = "bottom";
+      gravityToast = 'bottom';
     }
 
-    String images;
+    String? images;
     if (icon == ICON.CLOSE) {
-      images = "close";
+      images = 'close';
     } else if (icon == ICON.ERROR) {
-      images = "error";
+      images = 'error';
     } else if (icon == ICON.INFO) {
-      images = "info";
+      images = 'info';
     } else if (icon == ICON.SUCCESS) {
-      images = "success";
+      images = 'success';
     } else if (icon == ICON.WARNING) {
-      images = "warning";
+      images = 'warning';
     } else if (icon == ICON.ALARM) {
-      images = "alarm";
+      images = 'alarm';
     } else if (icon == ICON.LOCATION) {
-      images = "location";
+      images = 'location';
     } else if (icon == ICON.DND) {
-      images = "dnd";
+      images = 'dnd';
     } else if (icon == ICON.LOADING) {
-      images = "loading";
+      images = 'loading';
     } else {
       images = null;
     }
 
-    if (backgroundColor == null) {
-      backgroundColor = Colors.black;
-    }
-    if (textColor == null) {
-      textColor = Colors.white;
-    }
+    backgroundColor ??= Colors.black;
+    textColor ??= Colors.white;
 
-    if (imageSize == null) {
-      imageSize = 25;
-    }
+    imageSize ??= 25;
 
     /**
      * mapped input here with method channel for native changes.
      */
-    final Map<String, dynamic> parameters = <String, dynamic>{
+    final parameters = <String, dynamic>{
       'message': message,
       'length': toast,
-      'time': timeInSeconds != null ? timeInSeconds : 1,
+      'time': timeInSeconds ?? 1,
       'gravity': gravityToast,
       'icon': images,
-      'bgcolor': backgroundColor != null ? backgroundColor.value : Colors.black.value,
-      'textcolor': textColor != null ? textColor.value : Colors.white.value,
-      'fontSize': fontSize != null ? fontSize : 16.0,
-      'radius': radius != null ? radius : 5,
-      'elevation': elevation != null ? elevation : 5,
-      'imageSize': imageSize != null ? imageSize : 25,
+      'bgcolor': backgroundColor.value,
+      'textcolor': textColor.value,
+      'fontSize': fontSize ?? 16.0,
+      'radius': radius ?? 5,
+      'elevation': elevation ?? 5,
+      'imageSize': imageSize,
     };
 
-    bool res = await _channel.invokeMethod('showToast', parameters);
+    var res = await _channel.invokeMethod('showToast', parameters);
     return res;
   }
 }
